a225ac5a5f2c7418b3265aec65a8c5a50ff7377b
toshikingggg <study.king7777@gmail.com>
2021-11-01 01:52:46 +0900

    [add]three & cube sample

--- App.tsx
+++ App.tsx
@@ -1,21 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ExpoWebGLRenderingContext, GLView } from "expo-gl";
+import React from "react";
+import {
+  Scene,
+  Mesh,
+  PerspectiveCamera,
+  MeshBasicMaterial,
+  BoxGeometry,
+} from "three";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import { Renderer } from "expo-three";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const App: React.FC = () => {
+  const _onGLContextCreate = async (gl: ExpoWebGLRenderingContext) => {
+    const renderer = new Renderer({ gl });
+    renderer.setSize(gl.drawingBufferWidth, gl.drawingBufferHeight);
+
+    const scene = new Scene();
+    const camera = new PerspectiveCamera(
+      75,
+      gl.drawingBufferWidth / gl.drawingBufferHeight,
+      0.1,
+      1000
+    );
+    const geometry = new BoxGeometry(1, 1, 1);
+    const material = new MeshBasicMaterial({ color: "blue" });
+    const cube = new Mesh(geometry, material);
+    scene.add(cube);
+
+    camera.position.z = 5;
+
+    const animate = () => {
+      requestAnimationFrame(animate);
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+      renderer.render(scene, camera);
+      gl.endFrameEXP();
+    };
+    animate();
+  };
+  return <GLView style={{ flex: 1 }} onContextCreate={_onGLContextCreate} />;
+};
+
+export default App;
